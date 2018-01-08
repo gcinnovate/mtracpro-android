4c2f54eea51efaf73235dbec964494463ec75be4
eq62Roket <otimtony>
2018-01-08 15:33:25 +0300

    replaced getActionBar() method with getSupportActionBar() method to enable the functioning of the back arrow button

--- mTracPro/app/src/main/java/com/example/eq62roket/mtracpro/DeathActivity.java
+++ mTracPro/app/src/main/java/com/example/eq62roket/mtracpro/DeathActivity.java
@@ -1,15 +1,10 @@
 package com.example.eq62roket.mtracpro;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.PorterDuff;
-import android.os.Vibrator;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -17,14 +12,6 @@ import android.widget.Toast;
 
 public class DeathActivity extends AppCompatActivity {
     Button deathButton;
-    EditText death_ma, death_dy, death_sa, death_af, death_ae, death_ab, death_mg, death_ch, death_gw, death_me,
-            death_nt, death_vf, death_pl, death_tf, death_yf, death_tb;
-    Animation animShake;
-    public Vibrator vib;
-    TextInputLayout death_ma_label, death_dy_label, death_sa_label, death_af_label,
-            death_ae_label, death_ab_label, death_mg_label, death_ch_label, death_gw_label, death_yf_label,
-            death_me_label, death_nt_label, death_vf_label, death_pl_label, death_tf_label, death_tb_label;
-    LinearLayout death_linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,64 +19,25 @@ public class DeathActivity extends AppCompatActivity {
         setContentView(R.layout.activity_death);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        death_ma_label = (TextInputLayout) findViewById(R.id.death_ma_label);
-        death_dy_label = (TextInputLayout) findViewById(R.id.death_dy_label);
-        death_sa_label = (TextInputLayout) findViewById(R.id.death_sa_label);
-        death_af_label = (TextInputLayout) findViewById(R.id.death_af_label);
-        death_ae_label = (TextInputLayout) findViewById(R.id.death_ae_label);
-        death_ab_label = (TextInputLayout) findViewById(R.id.death_ab_label);
-        death_mg_label = (TextInputLayout) findViewById(R.id.death_mg_label);
-        death_ch_label = (TextInputLayout) findViewById(R.id.death_ch_label);
-        death_gw_label = (TextInputLayout) findViewById(R.id.death_gw_label);
-        death_yf_label = (TextInputLayout) findViewById(R.id.death_yf_label);
-        death_me_label = (TextInputLayout) findViewById(R.id.death_me_label);
-        death_nt_label = (TextInputLayout) findViewById(R.id.death_nt_label);
-        death_vf_label = (TextInputLayout) findViewById(R.id.death_vf_label);
-        death_pl_label = (TextInputLayout) findViewById(R.id.death_pl_label);
-        death_tf_label = (TextInputLayout) findViewById(R.id.death_tf_label);
-        death_tb_label = (TextInputLayout) findViewById(R.id.death_tb_label);
-
         deathButton = (Button) findViewById(R.id.deathButton);
-        deathButton.getBackground().setColorFilter(0xFF00FF00, PorterDuff.Mode.MULTIPLY);
-
-        death_ma = (EditText)findViewById(R.id.death_ma);
-        death_dy = (EditText)findViewById(R.id.death_dy);
-        death_sa = (EditText)findViewById(R.id.death_sa);
-        death_af = (EditText)findViewById(R.id.death_af);
-        death_ae = (EditText)findViewById(R.id.death_ae);
-        death_ab = (EditText)findViewById(R.id.death_ab);
-        death_mg = (EditText)findViewById(R.id.death_mg);
-        death_ch = (EditText)findViewById(R.id.death_ch);
-        death_gw = (EditText)findViewById(R.id.death_gw);
-        death_me = (EditText)findViewById(R.id.death_me);
-        death_nt = (EditText)findViewById(R.id.death_nt);
-        death_vf = (EditText)findViewById(R.id.death_vf);
-        death_pl = (EditText)findViewById(R.id.death_pl);
-        death_tf = (EditText)findViewById(R.id.death_tf);
-        death_yf = (EditText)findViewById(R.id.death_yf);
-        death_tb = (EditText)findViewById(R.id.death_tb);
-
-        animShake = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.shake);
-        vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
         deathButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!death_ma.getText().toString().equals("") || !death_dy.getText().toString().equals("")
-                        || !death_sa.getText().toString().equals("") || !death_af.getText().toString().equals("")
-                        || !death_ae.getText().toString().equals("") || !death_ae.getText().toString().equals("")
-                        || !death_ab.getText().toString().equals("") || !death_mg.getText().toString().equals("")
-                        || !death_ch.getText().toString().equals("") || !death_gw.getText().toString().equals("")
-                        || !death_me.getText().toString().equals("") || !death_yf.getText().toString().equals("")
-                        || !death_nt.getText().toString().equals("") || !death_tf.getText().toString().equals("")
-                        || !death_vf.getText().toString().equals("") || !death_pl.getText().toString().equals("")
-                        || !death_tb.getText().toString().equals("")) {
-                    Intent deathIntent = new Intent(DeathActivity.this, MainActivity.class);
-                    startActivity(deathIntent);
-                    finish();
-                }
-                else {
-                    Toast.makeText(DeathActivity.this, "Please input amount before submitting", Toast.LENGTH_LONG).show();
+                LinearLayout death_linearLayout = (LinearLayout)findViewById(R.id.death_linearLayout);
+                for (int i = 0; i < death_linearLayout.getChildCount(); i++ ){
+                    View view1 = death_linearLayout.getChildAt(i);
+                    if (view1 instanceof TextInputLayout){
+                        EditText editText = ((TextInputLayout) view1).getEditText();
+                        if (!editText.getText().toString().equals("")){
+                            Intent deathIntent = new Intent(DeathActivity.this, MainActivity.class);
+                            startActivity(deathIntent);
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(DeathActivity.this, "Please input a death before submitting", Toast.LENGTH_LONG).show();
+                        }
+                    }
                 }
             }
 
