8d001ae8ec55b9ed536e4861833111712a51a7ff
eq62Roket <otimtony>
2018-01-11 13:33:36 +0300

    Removed form validation methods and rewrote new methods

--- mTracPro/app/src/main/java/com/example/eq62roket/mtracpro/Activities/MatActivity.java
+++ mTracPro/app/src/main/java/com/example/eq62roket/mtracpro/Activities/MatActivity.java
@@ -26,9 +26,6 @@ public class MatActivity extends AppCompatActivity {
             mat_rdt_neg_treated, mat_rdt_pos_treated, mat_microscopy_neg_treated, mat_microscopy_pos_treated;
     Animation animShake;
     public Vibrator vib;
-    TextInputLayout mat_microscopy_pos_treated_label, mat_microscopy_neg_treated_label, mat_rdt_pos_treated_label, mat_rdt_neg_treated_label,
-            mat_not_tested_treated_label, mat_microscopy_positive_label, mat_microscopy_tested_label, mat_rdt_positive_label, mat_rdt_tested_label,
-            mat_suspected_malaria_label;
 
     private LinearLayout mat_linearLayout;
 
@@ -40,17 +37,6 @@ public class MatActivity extends AppCompatActivity {
 
         // setting up VolleyHelper
         mVolleyHelper = new VolleyHelper(this);
-
-        mat_microscopy_pos_treated_label = (TextInputLayout) findViewById(R.id.mat_microscopy_pos_treated_label);
-        mat_microscopy_neg_treated_label = (TextInputLayout) findViewById(R.id.mat_microscopy_neg_treated_label);
-        mat_rdt_pos_treated_label = (TextInputLayout) findViewById(R.id.mat_rdt_pos_treated_label);
-        mat_rdt_neg_treated_label = (TextInputLayout) findViewById(R.id.mat_rdt_neg_treated_label);
-        mat_not_tested_treated_label = (TextInputLayout) findViewById(R.id.mat_not_tested_treated_label);
-        mat_microscopy_positive_label = (TextInputLayout) findViewById(R.id.mat_microscopy_positive_label);
-        mat_microscopy_tested_label = (TextInputLayout) findViewById(R.id.mat_microscopy_tested_label);
-        mat_rdt_positive_label = (TextInputLayout) findViewById(R.id.mat_rdt_positive_label);
-        mat_rdt_tested_label = (TextInputLayout) findViewById(R.id.mat_rdt_tested_label);
-        mat_suspected_malaria_label = (TextInputLayout) findViewById(R.id.mat_suspected_malaria_label);
 
         mat_linearLayout = (LinearLayout) findViewById(R.id.mat_linearLayout);
 
@@ -81,197 +67,11 @@ public class MatActivity extends AppCompatActivity {
 
     private void submitForm() {
 
-        if (!checkSupectedMalaria()) {
-            mat_suspected_malaria.setAnimation(animShake);
-            mat_suspected_malaria.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
+        int ret = mVolleyHelper.sendData(mat_linearLayout);
+        if (ret == 0){
+            Intent matIntent = new Intent(MatActivity.this, MainActivity.class);
+            startActivity(matIntent);
+            finish();
         }
-        if (!checkRDTTested()) {
-            mat_rdt_tested.setAnimation(animShake);
-            mat_rdt_tested.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
-        }
-        if (!checkRDTPositive()) {
-            mat_rdt_positive.setAnimation(animShake);
-            mat_rdt_positive.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
-        }
-        if (!checkMicroscopyTested()) {
-            mat_microscopy_tested.setAnimation(animShake);
-            mat_microscopy_tested.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
-        }
-        if (!checkMicroscopyPositive()) {
-            mat_microscopy_positive.setAnimation(animShake);
-            mat_microscopy_positive.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
-        }
-        if (!checkNotTestedCasesTreated()) {
-            mat_not_tested_treated.setAnimation(animShake);
-            mat_not_tested_treated.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
-        }
-        if (!checkRDTNegativeTreated()) {
-            mat_rdt_neg_treated.setAnimation(animShake);
-            mat_rdt_neg_treated.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
-        }
-        if (!checkRDTPositiveTreated()) {
-            mat_rdt_pos_treated.setAnimation(animShake);
-            mat_rdt_pos_treated.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
-        }
-        if (!checkMicroscopyNegativeTreated()) {
-            mat_microscopy_neg_treated.setAnimation(animShake);
-            mat_microscopy_neg_treated.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
-        }
-        if (!checkMicroscopyPositiveTreated()) {
-            mat_microscopy_pos_treated.setAnimation(animShake);
-            mat_microscopy_pos_treated.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
-        }
-
-
-        mat_suspected_malaria_label.setErrorEnabled(false);
-        mat_rdt_tested_label.setErrorEnabled(false);
-        mat_rdt_positive_label.setErrorEnabled(false);
-        mat_microscopy_tested_label.setErrorEnabled(false);
-        mat_microscopy_positive_label.setErrorEnabled(false);
-        mat_not_tested_treated_label.setErrorEnabled(false);
-        mat_rdt_pos_treated_label.setErrorEnabled(false);
-        mat_microscopy_neg_treated_label.setErrorEnabled(false);
-        mat_microscopy_pos_treated_label.setErrorEnabled(false);
-        mat_rdt_neg_treated_label.setErrorEnabled(false);
-
-        mVolleyHelper.sendData(mat_linearLayout);
-
-        Intent aptIntent = new Intent(MatActivity.this, MainActivity.class);
-        startActivity(aptIntent);
-        finish();
-        Toast.makeText(getApplicationContext(), "Information has been submitted", Toast.LENGTH_SHORT).show();
     }
-
-    private boolean checkSupectedMalaria() {
-        if (mat_suspected_malaria.getText().toString().trim().isEmpty()) {
-
-            mat_suspected_malaria_label.setErrorEnabled(true);
-            mat_suspected_malaria_label.setError(getString(R.string.err_msg_mat_suspected_malaria_label));
-            return false;
-        }
-        mat_suspected_malaria_label.setErrorEnabled(false);
-        return true;
-    }
-
-    private boolean checkRDTTested() {
-        if (mat_rdt_tested.getText().toString().trim().isEmpty()) {
-
-            mat_rdt_tested_label.setErrorEnabled(true);
-            mat_rdt_tested_label.setError(getString(R.string.err_msg_mat_rdt_tested_label));
-            return false;
-        }
-        mat_rdt_tested_label.setErrorEnabled(false);
-        return true;
-
-    }
-
-    private boolean checkRDTPositive() {
-        if (mat_rdt_positive.getText().toString().trim().isEmpty()) {
-
-            mat_rdt_positive_label.setErrorEnabled(true);
-            mat_rdt_positive_label.setError(getString(R.string.err_msg_mat_rdt_positive_label));
-            return false;
-        }
-        mat_rdt_positive_label.setErrorEnabled(false);
-        return true;
-    }
-    private boolean checkMicroscopyTested() {
-        if (mat_microscopy_tested.getText().toString().trim().isEmpty()) {
-
-            mat_microscopy_tested_label.setErrorEnabled(true);
-            mat_microscopy_tested_label.setError(getString(R.string.err_msg_mat_microscopy_tested_label));
-            return false;
-        }
-        mat_microscopy_tested_label.setErrorEnabled(false);
-        return true;
-    }
-
-    private boolean checkMicroscopyPositive() {
-        if (mat_microscopy_positive.getText().toString().trim().isEmpty()) {
-
-            mat_microscopy_positive_label.setErrorEnabled(true);
-            mat_microscopy_positive_label.setError(getString(R.string.err_msg_mat_microscopy_positive_label));
-            return false;
-        }
-        mat_microscopy_positive_label.setErrorEnabled(false);
-        return true;
-
-    }
-
-    private boolean checkNotTestedCasesTreated() {
-        if (mat_not_tested_treated.getText().toString().trim().isEmpty()) {
-
-            mat_not_tested_treated_label.setErrorEnabled(true);
-            mat_not_tested_treated_label.setError(getString(R.string.err_msg_mat_not_tested_treated_label));
-            return false;
-        }
-        mat_not_tested_treated_label.setErrorEnabled(false);
-        return true;
-    }
-
-    private boolean checkRDTNegativeTreated() {
-        if (mat_rdt_neg_treated.getText().toString().trim().isEmpty()) {
-
-            mat_rdt_neg_treated_label.setErrorEnabled(true);
-            mat_rdt_neg_treated_label.setError(getString(R.string.err_msg_mat_rdt_neg_treated_label));
-            return false;
-        }
-        mat_rdt_neg_treated_label.setErrorEnabled(false);
-        return true;
-    }
-
-    private boolean checkRDTPositiveTreated() {
-        if (mat_rdt_pos_treated.getText().toString().trim().isEmpty()) {
-
-            mat_rdt_pos_treated_label.setErrorEnabled(true);
-            mat_rdt_pos_treated_label.setError(getString(R.string.err_msg_mat_rdt_pos_treated_label));
-            return false;
-        }
-        mat_rdt_pos_treated_label.setErrorEnabled(false);
-        return true;
-
-    }
-
-    private boolean checkMicroscopyNegativeTreated() {
-        if (mat_microscopy_neg_treated.getText().toString().trim().isEmpty()) {
-
-            mat_microscopy_neg_treated_label.setErrorEnabled(true);
-            mat_microscopy_neg_treated_label.setError(getString(R.string.err_msg_mat_microscopy_neg_treated_label));
-            return false;
-        }
-        mat_microscopy_neg_treated_label.setErrorEnabled(false);
-        return true;
-    }
-
-    private boolean checkMicroscopyPositiveTreated() {
-        if (mat_microscopy_pos_treated.getText().toString().trim().isEmpty()) {
-
-            mat_microscopy_pos_treated_label.setErrorEnabled(true);
-            mat_microscopy_pos_treated_label.setError(getString(R.string.err_msg_mat_microscopy_pos_treated_label));
-            return false;
-        }
-        mat_microscopy_pos_treated_label.setErrorEnabled(false);
-        return true;
-    }
-
 }
