f1c8dfc52677070ae0b85a78bfaebba8b7365067
eq62Roket <otimtony>
2018-01-13 15:10:49 +0300

    Deleted commented out code

--- mTracPro/app/src/main/java/com/example/eq62roket/mtracpro/Activities/HistoryActivity.java
+++ mTracPro/app/src/main/java/com/example/eq62roket/mtracpro/Activities/HistoryActivity.java
@@ -40,10 +40,6 @@ public class HistoryActivity extends AppCompatActivity {
         adapter = new RecyclerAdapter(arrayList, HistoryActivity.this);
         recyclerView.setAdapter(adapter);
 
-        search.setOnQueryTextListener(listener);
-
-
-//        HistoryData();
     }
 
 
@@ -54,74 +50,4 @@ public class HistoryActivity extends AppCompatActivity {
         return true;
     }
 
-    SearchView.OnQueryTextListener listener = new SearchView.OnQueryTextListener() {
-        @Override
-        public boolean onQueryTextChange(String query) {
-                query = query.toLowerCase();
-                final ArrayList<History> filteredList = new ArrayList<>();
-                for (int i = 0; i<arrayList.size(); i++) {
-                    final String text = arrayList.get(i).getDetail().toLowerCase();
-                if (text.contains(query)) {
-                    filteredList.add(arrayList.get(i));
-                }
-            }
-                recyclerView.setLayoutManager(new LinearLayoutManager(HistoryActivity.this));
-                    adapter = new RecyclerAdapter(filteredList, HistoryActivity.this );
-                    recyclerView.setAdapter(adapter);
-                    adapter.notifyDataSetChanged();  // data set changed<br />
-            return true;
-           }
-        public boolean onQueryTextSubmit(String query) {
-            return false;
-        }
-    };
-
-//    private void HistoryData() {
-//        History history = new History("12", "cases.ma.4.dy.5.ch.1",
-//                "You reported:\\n 4 cases of malari\\n 5 cases of dysentery and\\n 1 case of cholera",
-//                "2018-01-11 15:10");
-//        arrayList.add(history);
-//
-//        history = new History("4", "death.ab.3.tf.1",
-//                "You reported:\\n 3 deaths of animal bites\\n1 death of typhoid",
-//                "2018-01-11 13:11");
-//        arrayList.add(history);
-//
-//        history = new History("86", "cases.ma.0",
-//                "You reported:\\n 0 cases of malaria",
-//                "2018-01-11 12:10");
-//        arrayList.add(history);
-//
-//        history = new History("7", "death.ab.3.tf.1",
-//                "You reported:\\n 3 deaths of animal bites\\n1 death of typhoid",
-//                "2018-01-11 13:11");
-//        arrayList.add(history);
-//
-//        history = new History("8", "cases.ma.0",
-//                "You reported:\\n 0 cases of malaria",
-//                "2018-01-11 12:10");
-//        arrayList.add(history);
-//
-//        history = new History("54", "death.ab.3.tf.1",
-//                "You reported:\\n 3 deaths of animal bites\\n1 death of typhoid",
-//                "2018-01-11 13:11");
-//        arrayList.add(history);
-//
-//        history = new History("34", "cases.ma.0",
-//                "You reported:\\n 0 cases of malaria",
-//                "2018-01-11 12:10");
-//        arrayList.add(history);
-//
-//        history = new History("2", "death.ab.3.tf.1",
-//                "You reported:\\n 3 deaths of animal bites\\n1 death of typhoid",
-//                "2018-01-11 13:11");
-//        arrayList.add(history);
-//
-//        history = new History("34", "cases.ma.0",
-//                "You reported:\\n 0 cases of malaria",
-//                "2018-01-11 12:10");
-//        arrayList.add(history);
-//
-//        adapter.notifyDataSetChanged();
-//    }
 }
