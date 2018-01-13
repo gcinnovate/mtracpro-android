0eb46cf1be83a11af6d3d674a3c27c74481c43a3
eq62Roket <otimtony>
2018-01-13 14:58:04 +0300

    Refactored coded and replaced the older form validation methods with more efficient validation methods

--- mTracPro/app/src/main/java/com/example/eq62roket/mtracpro/Activities/TraActivity.java
+++ mTracPro/app/src/main/java/com/example/eq62roket/mtracpro/Activities/TraActivity.java
@@ -6,6 +6,7 @@ import android.os.Vibrator;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -26,8 +27,6 @@ public class TraActivity extends AppCompatActivity {
             tra_fansidar, tra_rdt_malaria;
     Animation animShake;
     public Vibrator vib;
-    TextInputLayout tra_rdt_malaria_label, tra_fansidar_label, tra_iv_artesunate_label, tra_depo_provera_label,
-            tra_amoxcilline_label, tra_measles_vaccine_label, tra_ors_sackets_label, tra_act_tablets_label;
 
     private LinearLayout tra_linearLinear;
 
@@ -39,15 +38,6 @@ public class TraActivity extends AppCompatActivity {
 
         // setting up VolleyHelper
         mVolleyHelper = new VolleyHelper(this);
-
-        tra_rdt_malaria_label = (TextInputLayout) findViewById(R.id.tra_rdt_malaria_label);
-        tra_fansidar_label = (TextInputLayout) findViewById(R.id.tra_fansidar_label);
-        tra_iv_artesunate_label = (TextInputLayout) findViewById(R.id.tra_iv_artesunate_label);
-        tra_depo_provera_label = (TextInputLayout) findViewById(R.id.tra_depo_provera_label);
-        tra_amoxcilline_label = (TextInputLayout) findViewById(R.id.tra_amoxcilline_label);
-        tra_measles_vaccine_label = (TextInputLayout) findViewById(R.id.tra_measles_vaccine_label);
-        tra_ors_sackets_label = (TextInputLayout) findViewById(R.id.tra_ors_sackets_label);
-        tra_act_tablets_label = (TextInputLayout) findViewById(R.id.tra_act_tablets_label);
 
         tra_linearLinear = (LinearLayout) findViewById(R.id.tra_linearLinear);
 
@@ -76,161 +66,92 @@ public class TraActivity extends AppCompatActivity {
     }
 
     private void submitForm() {
+        final String act_tablets = tra_act_tablets.getText().toString().trim();
+        final String ors_sackets = tra_ors_sackets.getText().toString().trim();
+        final String measles_vaccine = tra_measles_vaccine.getText().toString().trim();
+        final String amoxcilline = tra_amoxcilline.getText().toString().trim();
+        final String depo_provera = tra_depo_provera.getText().toString().trim();
+        final String iv_artesunate = tra_iv_artesunate.getText().toString().trim();
+        final String fansidar = tra_fansidar.getText().toString().trim();
+        final String rdt_malaria = tra_rdt_malaria.getText().toString().trim();
 
-        if (!checkMeaslesVaccine()) {
-            tra_measles_vaccine.setAnimation(animShake);
-            tra_measles_vaccine.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
-        }
-        if (!checkAmoxicilline()) {
-            tra_amoxcilline.setAnimation(animShake);
-            tra_amoxcilline.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
-        }
-        if (!checkDepoProvera()) {
-            tra_depo_provera.setAnimation(animShake);
-            tra_depo_provera.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
-        }
-        if (!checkIVArtesunate()) {
-            tra_iv_artesunate.setAnimation(animShake);
-            tra_iv_artesunate.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
-        }
-        if (!checkFansidar()) {
-            tra_fansidar.setAnimation(animShake);
-            tra_fansidar.startAnimation(animShake);
-            vib.vibrate(120);
-            return;
-        }
-        if (!checkACT()) {
+        if (TextUtils.isEmpty(act_tablets)){
+            tra_act_tablets.setError(getString(R.string.err_msg_tra_act_tablets_label));
+            tra_act_tablets.requestFocus();
             tra_act_tablets.setAnimation(animShake);
             tra_act_tablets.startAnimation(animShake);
             vib.vibrate(120);
             return;
         }
-        if (!checkORS()) {
+
+        if (TextUtils.isEmpty(ors_sackets)){
+            tra_ors_sackets.setError(getString(R.string.err_msg_tra_ors_sackets_label));
+            tra_ors_sackets.requestFocus();
             tra_ors_sackets.setAnimation(animShake);
             tra_ors_sackets.startAnimation(animShake);
             vib.vibrate(120);
             return;
         }
-        if (!checkRDT()) {
+
+        if (TextUtils.isEmpty(measles_vaccine)){
+            tra_measles_vaccine.setError(getString(R.string.err_msg_tra_measles_vaccine_label));
+            tra_measles_vaccine.requestFocus();
+            tra_measles_vaccine.setAnimation(animShake);
+            tra_measles_vaccine.startAnimation(animShake);
+            vib.vibrate(120);
+            return;
+        }
+
+        if (TextUtils.isEmpty(amoxcilline)){
+            tra_amoxcilline.setError(getString(R.string.err_msg_tra_amoxcilline_label));
+            tra_amoxcilline.requestFocus();
+            tra_amoxcilline.setAnimation(animShake);
+            tra_amoxcilline.startAnimation(animShake);
+            vib.vibrate(120);
+            return;
+        }
+
+        if (TextUtils.isEmpty(depo_provera)){
+            tra_depo_provera.setError(getString(R.string.err_msg_tra_depo_provera_label));
+            tra_depo_provera.requestFocus();
+            tra_depo_provera.setAnimation(animShake);
+            tra_depo_provera.startAnimation(animShake);
+            vib.vibrate(120);
+            return;
+        }
+
+        if (TextUtils.isEmpty(iv_artesunate)){
+            tra_iv_artesunate.setError(getString(R.string.err_msg_tra_iv_artesunate_label));
+            tra_iv_artesunate.requestFocus();
+            tra_iv_artesunate.setAnimation(animShake);
+            tra_iv_artesunate.startAnimation(animShake);
+            vib.vibrate(120);
+            return;
+        }
+
+        if (TextUtils.isEmpty(fansidar)){
+            tra_fansidar.setError(getString(R.string.err_msg_tra_fansidar_label));
+            tra_fansidar.requestFocus();
+            tra_fansidar.setAnimation(animShake);
+            tra_fansidar.startAnimation(animShake);
+            vib.vibrate(120);
+            return;
+        }
+
+        if (TextUtils.isEmpty(rdt_malaria)){
+            tra_rdt_malaria.setError(getString(R.string.err_msg_tra_rdt_malaria_label));
+            tra_rdt_malaria.requestFocus();
             tra_rdt_malaria.setAnimation(animShake);
             tra_rdt_malaria.startAnimation(animShake);
             vib.vibrate(120);
             return;
         }
 
-
-        tra_measles_vaccine_label.setErrorEnabled(false);
-        tra_act_tablets_label.setErrorEnabled(false);
-        tra_ors_sackets_label.setErrorEnabled(false);
-        tra_amoxcilline_label.setErrorEnabled(false);
-        tra_depo_provera_label.setErrorEnabled(false);
-        tra_rdt_malaria_label.setErrorEnabled(false);
-        tra_fansidar_label.setErrorEnabled(false);
-        tra_iv_artesunate_label.setErrorEnabled(false);
-
-        mVolleyHelper.sendData(tra_linearLinear);
-
-        Intent aptIntent = new Intent(TraActivity.this, MainActivity.class);
-        startActivity(aptIntent);
-        finish();
-        Toast.makeText(getApplicationContext(), "Information has been submitted", Toast.LENGTH_SHORT).show();
-    }
-
-    private boolean checkMeaslesVaccine() {
-        if (tra_measles_vaccine.getText().toString().trim().isEmpty()) {
-
-            tra_measles_vaccine_label.setErrorEnabled(true);
-            tra_measles_vaccine_label.setError(getString(R.string.err_msg_tra_measles_vaccine_label));
-            return false;
+        int ret = mVolleyHelper.sendData(tra_linearLinear);
+        if (ret == 0){
+            Intent traIntent = new Intent(TraActivity.this, MainActivity.class);
+            startActivity(traIntent);
+            finish();
         }
-        tra_measles_vaccine_label.setErrorEnabled(false);
-        return true;
     }
-
-    private boolean checkAmoxicilline() {
-        if (tra_amoxcilline.getText().toString().trim().isEmpty()) {
-
-            tra_amoxcilline_label.setErrorEnabled(true);
-            tra_amoxcilline_label.setError(getString(R.string.err_msg_tra_amoxcilline_label));
-            return false;
-        }
-        tra_amoxcilline_label.setErrorEnabled(false);
-        return true;
-
-    }
-
-    private boolean checkDepoProvera() {
-        if (tra_depo_provera.getText().toString().trim().isEmpty()) {
-
-            tra_depo_provera_label.setErrorEnabled(true);
-            tra_depo_provera_label.setError(getString(R.string.err_msg_tra_depo_provera_label));
-            return false;
-        }
-        tra_depo_provera_label.setErrorEnabled(false);
-        return true;
-    }
-    private boolean checkIVArtesunate() {
-        if (tra_iv_artesunate.getText().toString().trim().isEmpty()) {
-
-            tra_iv_artesunate_label.setErrorEnabled(true);
-            tra_iv_artesunate_label.setError(getString(R.string.err_msg_tra_iv_artesunate_label));
-            return false;
-        }
-        tra_iv_artesunate_label.setErrorEnabled(false);
-        return true;
-    }
-
-    private boolean checkFansidar() {
-        if (tra_fansidar.getText().toString().trim().isEmpty()) {
-
-            tra_fansidar_label.setErrorEnabled(true);
-            tra_fansidar_label.setError(getString(R.string.err_msg_tra_fansidar_label));
-            return false;
-        }
-        tra_fansidar_label.setErrorEnabled(false);
-        return true;
-    }
-
-    private boolean checkORS() {
-        if (tra_ors_sackets.getText().toString().trim().isEmpty()) {
-
-            tra_ors_sackets_label.setErrorEnabled(true);
-            tra_ors_sackets_label.setError(getString(R.string.err_msg_tra_ors_sackets_label));
-            return false;
-        }
-        tra_ors_sackets_label.setErrorEnabled(false);
-        return true;
-
-    }
-
-    private boolean checkACT() {
-        if (tra_act_tablets.getText().toString().trim().isEmpty()) {
-
-            tra_act_tablets_label.setErrorEnabled(true);
-            tra_act_tablets_label.setError(getString(R.string.err_msg_tra_act_tablets_label));
-            return false;
-        }
-        tra_act_tablets_label.setErrorEnabled(false);
-        return true;
-    }
-    private boolean checkRDT() {
-        if (tra_rdt_malaria.getText().toString().trim().isEmpty()) {
-
-            tra_rdt_malaria_label.setErrorEnabled(true);
-            tra_rdt_malaria_label.setError(getString(R.string.err_msg_tra_rdt_malaria_label));
-            return false;
-        }
-        tra_rdt_malaria_label.setErrorEnabled(false);
-        return true;
-    }
-
-
 }
