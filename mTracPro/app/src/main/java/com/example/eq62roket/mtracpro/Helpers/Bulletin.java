b6262dacd666f0016e8ff194f78dd6a040629cca
eq62Roket <otimtony>
2018-01-24 12:00:26 +0300

    Java object to hold elements of date, id and content

--- mTracPro/app/src/main/java/com/example/eq62roket/mtracpro/Helpers/Bulletin.java
+++ mTracPro/app/src/main/java/com/example/eq62roket/mtracpro/Helpers/Bulletin.java
@@ -5,4 +5,35 @@ package com.example.eq62roket.mtracpro.Helpers;
  */
 
 public class Bulletin {
+    private String id, date, content;
+
+    public Bulletin(String id, String date, String content){
+        this.setId(id);
+        this.setDate(date);
+        this.setContent(content);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
